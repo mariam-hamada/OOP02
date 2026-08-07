2a3ad566b3508ba18475eef2f66571719cdd1a4e
mariam-hamada <168047165+mariam-hamada@users.noreply.github.com>
2026-08-07 23:38:49 +0300

    part 01 : Theoretical Questions , Question 1

--- Assignment6/Program.cs
+++ Assignment6/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 : Theoretical Questions
+
+            #region Question 1
+
+            //a) What is the difference between a class and a struct?
+            //class:is a refrence type stored in heap, when Assignment it copies the refrence, support inhertance ,can be null , default constructor is available if none is defined, suitable for complex programs.
+            //struct:is a value type stored in stack,  when assignment it copies the value, deosn't support inhertance ,cannot be null, default constructor is available all time, suitable for simple program.
+
+            //b) Why are classes more suitable than structs for large application
+            //Because classes support complex program as it support inhertance and avoids unnecessaray coping.
+            #endregion
+
+           
+            #endregion
+
+
         }
     }
 }
